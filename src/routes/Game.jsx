04337b4b1e4f7ba1e961f42414a1a8604ea5c161
José Leite <jf.leiteee@gmail.com>
2024-09-02 21:32:18 -0300

    routes created

---
+++ src/routes/Game.jsx
@@ -0,0 +1,33 @@
+import Questions from "../components/Questions"
+import Answers from "../components/Answers"
+import { VariableContext } from "../context/variableContext"
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function Game() {
+
+    const {failed, finished} = useContext(VariableContext)
+
+    const navigate = useNavigate()
+
+    function defeat() {
+        if(failed) {
+            navigate("/defeat")
+        }
+    }
+    function victory() {
+        if(finished) {
+            navigate("/victory")
+        }
+    }
+    
+    defeat()
+    victory()
+
+    return(
+        <>
+            <Questions /* componente de questoes *//>
+            <Answers /* componente de respostas *//>
+        </>
+    )
+}
