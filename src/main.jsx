0b98242871270a5170a81c2c3502855337056084
José Leite <jf.leiteee@gmail.com>
2024-09-02 21:32:59 -0300

    add: context

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,39 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+import Start from './components/Start.jsx'
+import Game from './routes/Game.jsx'
+import FinalScreen from './components/FinalScreen.jsx'
+import Failed from './components/Failed.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Start />
+  },
+  {
+    path: "/play",
+    element: <Game />
+  },
+  {
+    path: "/defeat",
+    element: <Failed />
+  }, 
+  {
+    path: "/victory",
+    element: <FinalScreen />
+  }
+])
+
+import { ContextProvider } from "./context/variableContext.jsx"
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ContextProvider>
+    <RouterProvider router={router} />
+  </ContextProvider>
 )
